e57b3f6bb45dc254731448f4c0d77bea3af376d4
Mahnoor Cheema <cheema.mahnoor@gmail.com>
2021-03-01 23:44:39 -0500

    Gets background color based on depth

--- src/components/ArtistSummary.js
+++ src/components/ArtistSummary.js
@@ -11,15 +11,24 @@ const getSmallestImage = images => {
     return smallestSoFar;
 }
 
+const getBackgroundColor = (depth) => {
+    const hue = 20 * depth
+    const saturation = 100
+    const lightness = 77
+    return `hsl(${hue}, ${saturation}%, ${lightness}%)`
+}
+
 /**
  * @param {Object} props
  * @param {Artist} props.artist
  * @param {ConnectedTrack} props.connectedTracks
+ * @param {number} props.depth
  */
-const ArtistSummary = ({ artist, connectedTracks }) => {
+const ArtistSummary = ({ artist, connectedTracks, depth=1 }) => {
     /** @type{[(Artist[] | null), Function]} */
     const [relatedArtists, setRelatedArtists] = useState(null)
     const {isArtistsUnique, addToSeenArtists} = useSeenArtists()
+
     const handleGetRelatedArtists = async () => {
         // const allRelatedArtists = await getFeaturedArtists(artist.id)
         // const uniqueRelatedArtists = allRelatedArtists.filter(isArtistsUnique)
@@ -27,14 +36,12 @@ const ArtistSummary = ({ artist, connectedTracks }) => {
         // setRelatedArtists(uniqueRelatedArtists)
         
         setRelatedArtists( await getFeaturedArtists(artist.id))
-        // console.log(relatedArtists)
     }
 
     const smallestImage = getSmallestImage(artist.images);
     const smallestAlbumImage = connectedTracks ? getSmallestImage(connectedTracks[0].album.images) : null
-    console.log(smallestAlbumImage)
     return (
-        <div id="tree" className="background-color">
+        <div id="tree">
             <div className="artist-summary">
                 {smallestImage ? <img className="artist-summary--img__circle" src={smallestImage.url} alt=""/> : <div className="circle"></div>}
                 <div className="artist-summary--details">
@@ -51,12 +58,12 @@ const ArtistSummary = ({ artist, connectedTracks }) => {
             
             {relatedArtists
                 ? <> 
-                    <ul className="related-artists--ul__style">
-                        <h3 className="related-artists--h3">
+                    <h3 className="related-artists--h3" style={{backgroundColor: getBackgroundColor(depth)}}>
                             Related Artists to {artist.name} ({relatedArtists.length})
-                        </h3>
+                    </h3>
+                    <ul className="related-artists--ul__style" style={{backgroundColor: getBackgroundColor(depth)}}>
                       {relatedArtists.map(artist => (
-                          <li key={artist.id}><ArtistSummary artist={artist.artist} connectedTracks={artist.connectedTracks}/></li>
+                          <li key={artist.id}><ArtistSummary artist={artist.artist} connectedTracks={artist.connectedTracks} depth={depth+1}/></li>
                       ))}  
                     </ul>
                 </>
